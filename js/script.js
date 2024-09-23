74f41c07dfe44798f00cb9a62b6e63a676502947
s-h-rumon <shahidhasanruman3@gmil.com>
2024-09-23 10:55:28 +0600

    confirmation modal completed

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const confModal = document.getElementById('confirmation-modal');
+
+// window.addEventListener(
+//   'scroll',
+//   function () {
+//     window.scrollTo(0, 0);
+//   },
+//   { passive: false }
+// );
